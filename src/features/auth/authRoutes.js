4d4fa1cfacf2c12b04df79e68a4fbb858baae19c
unknown <nesad.omeragich27@gmail.com>
2021-07-09 05:44:11 +0200

    Feat: add auth routes

--- src/features/auth/authRoutes.js
+++ src/features/auth/authRoutes.js
@@ -1,9 +1,13 @@
 import express from 'express';
 
+import auth from '../../middleware/auth';
+
 import authController from './authController';
 
 const router = express.Router();
 
-router.get('/signup', authController.signup);
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+router.post('/user', auth, authController.user);
 
 export default router;
