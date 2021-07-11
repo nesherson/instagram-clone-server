42cc43f1ef18a6db7cfe479a78a4022135bcdc40
unknown <nesad.omeragich27@gmail.com>
2021-07-11 19:43:00 +0200

    Refactor: change in file name

---
+++ src/features/user/userModel.js
@@ -0,0 +1,34 @@
+import { DataTypes } from 'sequelize';
+
+import sequelize from '../../db';
+
+const User = sequelize.define('user', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  fullname: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  profileImg: {
+    type: DataTypes.STRING,
+  },
+});
+
+export default User;
