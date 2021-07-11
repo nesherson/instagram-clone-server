08b8c000ac8f69fd8a9deec4bcbc3287a9bf6304
unknown <nesad.omeragich27@gmail.com>
2021-07-11 19:40:07 +0200

    Refactor: change in variable names

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -10,8 +10,8 @@ function auth(req, res, next) {
   }
 
   try {
-    const decoded = jwt.verify(token, JWT_SECRET);
-    req.user = decoded;
+    const decodedToken = jwt.verify(token, JWT_SECRET);
+    req.userData = { id: decodedToken.id };
     next();
   } catch (err) {
     res.status(400).send({ msg: 'Token is not valid' });
