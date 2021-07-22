5750db0324134960f05644c6053656b40cda9ea5
nesherson <nesad.omeragich27@gmail.com>
2021-07-22 17:29:21 +0200

    Feat: add route and controller for getting saved posts

--- src/features/user/userController.js
+++ src/features/user/userController.js
@@ -1,47 +1,54 @@
+import Post from "../post/postModel";
+
 import postDAL from "../post/postDAL";
 import commentDAL from "../comment/commentDAL";
 import userDAL from "./userDAL";
 import savedPostDAL from "../savedPost/savedPostDAL";
+import postLikeDAL from "../postLike/postLikeDAL";
 
 async function getUser(req, res) {
-  const users = await userDAL.findAll({
-    where: { id: req.userData.id },
-    attributes: { exclude: ["password"] },
-  });
+  try {
+    const users = await userDAL.findAll({
+      where: { id: req.userData.id },
+      attributes: { exclude: ["password"] },
+    });
 
-  const user = users[0];
+    const user = users[0];
 
-  const posts = await postDAL.findAll({ where: { userId: user.id } });
+    const posts = await postDAL.findAll({ where: { userId: user.id } });
 
-  const comments = await commentDAL.findAll();
+    const comments = await commentDAL.findAll();
 
-  const updatedPosts = posts.map((post) => {
-    const postComments = comments.filter(
-      (comment) => comment.postId === post.id
-    );
-    return {
-      id: post.id,
-      imageUrl: post.imageUrl,
-      caption: post.caption,
-      likes: post.likes,
-      createdAt: post.createdAt,
-      userId: post.userId,
-      comments: postComments,
+    const updatedPosts = posts.map((post) => {
+      const postComments = comments.filter(
+        (comment) => comment.postId === post.id
+      );
+      return {
+        id: post.id,
+        imageUrl: post.imageUrl,
+        caption: post.caption,
+        likes: post.likes,
+        createdAt: post.createdAt,
+        userId: post.userId,
+        comments: postComments,
+      };
+    });
+
+    const response = {
+      user: {
+        id: user.id,
+        email: user.email,
+        fullname: user.fullname,
+        username: user.username,
+        profileImg: user.profileImg,
+        posts: updatedPosts,
+      },
     };
-  });
 
-  const response = {
-    user: {
-      id: user.id,
-      email: user.email,
-      fullname: user.fullname,
-      username: user.username,
-      profileImg: user.profileImg,
-      posts: updatedPosts,
-    },
-  };
-
-  res.send(response);
+    res.status(200).send(response);
+  } catch (err) {
+    res.status(400).send({ msg: error.message });
+  }
 }
 
 async function savePost(req, res) {
@@ -88,6 +95,49 @@ async function savePost(req, res) {
   }
 }
 
-export { getUser, savePost };
+async function getSavedPosts(req, res) {
+  try {
+    const userId = req.userData.id;
 
-export default { getUser, savePost };
+    const savedPosts = await savedPostDAL.findAll({
+      where: { userId: userId },
+      include: [{ model: Post, attributes: ["id", "imageUrl"] }],
+    });
+
+    const postsLikes = await postLikeDAL.findAll();
+    const postsComments = await commentDAL.findAll();
+
+    const updatedSavedPosts = savedPosts.map((savedPost) => {
+      const savedPostLikes = postsLikes.filter(
+        (like) => like.postId === savedPost.postId
+      );
+      const savedPostComments = postsComments.filter(
+        (comment) => comment.postId === savedPost.postId
+      );
+
+      return {
+        id: savedPost.id,
+        post: {
+          id: savedPost.post.id,
+          userId: savedPost.post.userId,
+          imageUrl: savedPost.post.imageUrl,
+          likes: savedPostLikes,
+          comments: savedPostComments,
+        },
+      };  
+    });
+
+    const response = {
+      savedPosts: updatedSavedPosts,
+      msg: "Saved posts returned successfully.",
+    };
+
+    res.status(200).send(response);
+  } catch (err) {
+    res.status(400).send({ msg: error.message });
+  }
+}
+
+export { getUser, savePost, getSavedPosts };
+
+export default { getUser, savePost, getSavedPosts };
--- src/features/user/userRoutes.js
+++ src/features/user/userRoutes.js
@@ -6,8 +6,10 @@ import userController from './userController';
 
 const router = express.Router();
 
-router.get('/', auth, userController.getUser);
+router.get('/user', auth, userController.getUser);
 router.post('/user/:uid/save-post', auth, userController.savePost);
+router.get('/user/:uid/saved-posts', auth, userController.getSavedPosts);
+
 
 
 export default router;
